4a7b18d6514072fdce93142662b18622f08c9438
Cody & Don Lang <pair+ckendall.donlang@devbootcamp.com>
2016-03-03 20:04:05 -0800

    add views for tweets and users

--- sinatra-skeleton-mvc-challenge-master/app/controllers/users.rb
+++ sinatra-skeleton-mvc-challenge-master/app/controllers/users.rb
@@ -1,5 +1,28 @@
+get '/' do
+  erb :'users/index'
+end
+
+post 'users/login' do
+  @user = User.find_by(user_name: params[:user_name])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect "/"
+  end
+end
+
+get '/users/signup' do
+  erb :'/users/new'
+end
+
+post 'users/signup' do
+  User.create(params[:user])
+  redirect "/"
+end
+
 get '/users' do
- # display all records
+
 end
 
 get '/users/new' do
--- sinatra-skeleton-mvc-challenge-master/app/models/user.rb
+++ sinatra-skeleton-mvc-challenge-master/app/models/user.rb
@@ -1,3 +1,17 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypts::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
 end
