0bde883d69085b36ab0f6bc3c9665cb2e4f5ad14
Cody <email": null>
2016-03-04 15:10:35 -0800

    add button for profile

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,18 +8,15 @@ end
 
 get '/users/:id' do
   @user = User.find(params[:id])
-
   # @tweets = []
   # @user.posters.each do |poster|
 
   #   @tweets << Tweet.find_by(poster: poster.id)
   # end
 
-
-
   @tweets = []
   @user.posters.each do |poster|
-    temp = {}
+    # temp = {}
     name = User.find(poster.id)
     p "$" * 50
     p name
@@ -32,9 +29,6 @@ get '/users/:id' do
     # temp = {name: name.user_name, body: tweet.body}
     # @tweets << temp
   end
-
-
-
 
   erb :'/users/show'
 end
